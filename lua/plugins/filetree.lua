151fe92c0df6ca522e2b8b18c6b7f97a6f66f44d
wangtengfei <wtf@xiaoroujiankang.com>
2026-01-22 11:11:58 +0800

    修改目录树配置

--- lua/plugins/filetree.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "nvim-neo-tree/neo-tree.nvim",
-    config = function()
-        require("neo-tree").setup({})
-        -- vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
-    end
-}
-
--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,38 +1,39 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("nvim-tree").setup({
-      view = {
-        width = 30,
-        side = "left",
-      },
-      renderer = {
-        group_empty = true,
-        highlight_git = true,
-        icons = {
-          show = {
-            git = true,
-            folder = true,
-            file = true,
-            folder_arrow = true,
-          },
-        },
-      },
-      filters = {
-        dotfiles = false,
-      },
-      update_focused_file = {
-        enable = true,
-        update_root = false,
-      },
-      actions = {
-        open_file = {
-          quit_on_open = false, -- 打开文件时不自动关闭 tree
-        },
-      },
-    })
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("nvim-tree").setup({
+            view = {
+                width = 30,
+                side = "left",
+            },
+            renderer = {
+                group_empty = true,
+                highlight_git = true,
+                icons = {
+                    show = {
+                        git = true,
+                        folder = true,
+                        file = true,
+                        folder_arrow = true,
+                    },
+                },
+            },
+            filters = {
+                dotfiles = false,
+                git_ignored = false,
+            },
+            update_focused_file = {
+                enable = true,
+                update_root = false,
+            },
+            actions = {
+                open_file = {
+                    quit_on_open = false, -- 打开文件时不自动关闭 tree
+                },
+            },
+        })
 
-    vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
-  end
+        vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+    end
 }

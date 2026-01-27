3843a991b2f71d5baff91ee7cdf92db77c84840f
wangtengfei <wtf@xiaoroujiankang.com>
2026-01-27 16:38:35 +0800

    remove phpstan-lint

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -7,7 +7,7 @@ return {
             local lint = require("lint")
 
             lint.linters_by_ft = {
-                php = { "phpstan" },
+                php = {}, -- { "phpstan" },
                 javascript = { "eslint_d" },
                 typescript = { "eslint_d" },
                 python = { "flake8" },
